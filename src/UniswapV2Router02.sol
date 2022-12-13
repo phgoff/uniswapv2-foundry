d786f3056f3c375c433f504059de542b4a9e1404
goff <mous17zy@hotmail.com>
2022-12-13 23:02:21 +0700

    add test mint token

--- src/UniswapV2Router02.sol
+++ src/UniswapV2Router02.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.6;
+pragma solidity ^0.8.13;
 
 import './interfaces/IUniswapV2Factory.sol';
 import './interfaces/IUniswapV2Router02.sol';
@@ -11,15 +11,15 @@ import "./libraries/TransferHelper.sol";
 contract UniswapV2Router02 is IUniswapV2Router02 {
     using SafeMath for uint;
 
-    address public immutable override factory;
-    address public immutable override WETH;
+    address public immutable factory;
+    address public immutable WETH;
 
     modifier ensure(uint deadline) {
         require(deadline >= block.timestamp, 'UniswapV2Router: EXPIRED');
         _;
     }
 
-    constructor(address _factory, address _WETH) public {
+    constructor(address _factory, address _WETH) {
         factory = _factory;
         WETH = _WETH;
     }
@@ -148,7 +148,7 @@ contract UniswapV2Router02 is IUniswapV2Router02 {
         bool approveMax, uint8 v, bytes32 r, bytes32 s
     ) external virtual override returns (uint amountA, uint amountB) {
         address pair = UniswapV2Library.pairFor(factory, tokenA, tokenB);
-        uint value = approveMax ? uint(-1) : liquidity;
+        uint value = approveMax ? type(uint112).max : liquidity;
         IUniswapV2Pair(pair).permit(msg.sender, address(this), value, deadline, v, r, s);
         (amountA, amountB) = removeLiquidity(tokenA, tokenB, liquidity, amountAMin, amountBMin, to, deadline);
     }
@@ -162,7 +162,7 @@ contract UniswapV2Router02 is IUniswapV2Router02 {
         bool approveMax, uint8 v, bytes32 r, bytes32 s
     ) external virtual override returns (uint amountToken, uint amountETH) {
         address pair = UniswapV2Library.pairFor(factory, token, WETH);
-        uint value = approveMax ? uint(-1) : liquidity;
+        uint value = approveMax ? type(uint112).max : liquidity;
         IUniswapV2Pair(pair).permit(msg.sender, address(this), value, deadline, v, r, s);
         (amountToken, amountETH) = removeLiquidityETH(token, liquidity, amountTokenMin, amountETHMin, to, deadline);
     }
@@ -199,7 +199,7 @@ contract UniswapV2Router02 is IUniswapV2Router02 {
         bool approveMax, uint8 v, bytes32 r, bytes32 s
     ) external virtual override returns (uint amountETH) {
         address pair = UniswapV2Library.pairFor(factory, token, WETH);
-        uint value = approveMax ? uint(-1) : liquidity;
+        uint value = approveMax ? type(uint112).max : liquidity;
         IUniswapV2Pair(pair).permit(msg.sender, address(this), value, deadline, v, r, s);
         amountETH = removeLiquidityETHSupportingFeeOnTransferTokens(
             token, liquidity, amountTokenMin, amountETHMin, to, deadline
--- test/UniswapV2.t.sol
+++ test/UniswapV2.t.sol
@@ -3,31 +3,37 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/test/WETH9.sol";
+import "../src/test/ERC20.sol";
 import "../src/UniswapV2Factory.sol";
-// import "../src/UniswapV2Router02.sol";
+import "../src/UniswapV2Router02.sol";
 
 contract CounterTest is Test {
     WETH9 public weth;
+    ERC20 public dai;
+    ERC20 public usdc;
     UniswapV2Factory public factory;
-    // UniswapV2Router02 public router;
-    address feeToSetter = address(0x1);
+    UniswapV2Router02 public router;
+    address owner = address(0x1);
+    address trader = address(0x2);
 
     function setUp() public {
         weth = new WETH9();
-        factory = new UniswapV2Factory(feeToSetter);
-        // router = new UniswapV2Router02(factory, weth);
+        factory = new UniswapV2Factory(owner);
+        router = new UniswapV2Router02(address(factory), address(weth));
        
     }
-
-    function testIncrement() public {
-         emit log_address(address(factory));
-         emit log_address(address(weth));
-           emit log_address(factory.feeToSetter());
-        //  emit log_address(address(router));
+    function testMintToken() public {
+        vm.startPrank(owner);
+        dai = new ERC20(100);
+        uint256 balanceOfDai = dai.balanceOf(owner);
+        assertEq(balanceOfDai, 100);
     }
 
-    // function testSetNumber(uint256 x) public {
-    //     counter.setNumber(x);
-    //     assertEq(counter.number(), x);
-    // }
+    function testAddLP() public {
+        emit log_address(address(factory));
+        emit log_address(address(weth));
+        emit log_address(address(router));
+        emit log_address(factory.feeToSetter());
+
+    }
 }
