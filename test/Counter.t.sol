af4cb59bf8e10e5ac623b4397c7b25fea5e28566
goff <mous17zy@hotmail.com>
2022-12-13 21:48:20 +0700

    add uniswap

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -10,6 +10,7 @@ contract CounterTest is Test {
     function setUp() public {
         counter = new Counter();
         counter.setNumber(0);
+        emit log_address(address(counter));
     }
 
     function testIncrement() public {
